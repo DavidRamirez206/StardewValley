753030b8b4605682511f593ac4aa2436582c3ebb
ismaelbarrionuevo2006@gmail.com <ismaelbarrionuevo2006@gmail.com>
2024-08-29 16:30:58 -0500

    Classes and model created

---
+++ SV_Project/src/main/java/control/Controller.java
@@ -0,0 +1,20 @@
+package control;
+
+import java.util.List;
+import model.*;
+
+public class Controller {
+    private Chest chest;
+
+    public Controller(){
+        this.chest = new Chest();        
+    }
+
+    private Chest createChest(){
+        return chest;
+    }
+
+    public void addChest(Chest chest){
+
+    }
+}
---
+++ SV_Project/src/main/java/model/SpringCrop.java
@@ -0,0 +1,7 @@
+package model;
+
+public class SpringCrop extends Crop{
+    public SpringCrop(String name, Season season, State state, int growthTime){
+        super();
+    }
+}
