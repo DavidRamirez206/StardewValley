e613e1eb5101577bd7402df041387e4c4a0ea66e
ismaelbarrionuevo2006@gmail.com <ismaelbarrionuevo2006@gmail.com>
2024-09-06 16:45:30 -0500

    Menú por temporada hecho. Por cada minuto, la epoca del año cambia a la siguiente

--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -2,15 +2,84 @@ package control;
 
 import model.*;
 import structures.SimpleLinkedListChest;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Controller {
     private SimpleLinkedListChest chests;
+    private Season currentSeason;
+    private Thread seasonChangeThread;
 
     public Controller(){
         chests = new SimpleLinkedListChest();
+        this.currentSeason = Season.SPRING;  // Initial season
+        startSeasonChange();
+
+    }
+
+    private void startSeasonChange() {
+        seasonChangeThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(1 * 60 * 1000);  // Wait 1 minute (5 * 60 * 1000 ms)
+                        changeSeason();
+                    } catch (InterruptedException e) {
+                        throw new IllegalArgumentException("Season change interrupted.");
+                    }
+                }
+            }
+        });
+        seasonChangeThread.start();  // Start the thread
+    }
+
+    // Method to change to the next season
+    private void changeSeason() {
+        switch (currentSeason) {
+            case SPRING:
+                currentSeason = Season.SUMMER;
+                break;
+            case SUMMER:
+                currentSeason = Season.FALL;
+                break;
+            case FALL:
+                currentSeason = Season.WINTER;
+                break;
+            case WINTER:
+                currentSeason = Season.SPRING;
+                break;
+            case ANY_SEASON:
+                currentSeason = Season.ANY_SEASON;
+                break;
+        }
+    }
+
+    // Getter for the current season
+    public int currentSeason(){
+        int season = 0;
+        if (currentSeason.equals(Season.SPRING)) {
+            season = 1;
+        } else if (currentSeason.equals(Season.SUMMER)) {
+            season = 2;
+        } else if (currentSeason.equals(Season.FALL)) {
+            season = 3;
+        } else if (currentSeason.equals(Season.WINTER)) {
+            season = 4;
+        } else {
+            season = 5;
+        }
+        return season;
+    }
+
+
+    public Season getCurrentSeason() {
+        return currentSeason;
+    }
+
+    // Method to stop the thread (when the program ends)
+    public void stopThread() {
+        if (seasonChangeThread != null && seasonChangeThread.isAlive()) {
+            seasonChangeThread.interrupt();
+        }
     }
 
     public void createChest(String chestNumber){
--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -20,12 +20,12 @@ public class Main {
 	}
 
     public void run(){
-        boolean mySwitch = false;
 
         while (!mySwitch) {
             
             int option;
 
+            System.out.println("----------------------------------------------------------------------");
             System.out.println("\nWelcome to Stardew Valley. Select an option\n");
             System.out.println("\n 1) Create a chest \n 2) Add a stack to a chest \n 3) Add a crop to a stack");
             System.out.println("\n11) Test case \n12) Exit");
@@ -40,18 +40,81 @@ public class Main {
                     addStackToChest();
                     break;
                 case 3:
+                    System.out.println("----------------------------------------------------------------------");
+                    System.out.println("\nPress 'Enter' to show the menu based on the current season...");
+                    in.nextLine();  // Wait for user input to show the next menu
+                    menuBySeason();
+
+                    
                     addCropToStack();
                     break;
                 case 11:
                     testCase();
                     break;
                 case 12:
-                    mySwitch = true;
+                    exitProgram();
                     break;
                 default:
                     break;
             }
         }
+    }
+
+    // Method to exit the program
+    private void exitProgram() {
+        mySwitch = true;  // Set to true to exit the loop
+        controller.stopThread();  // Stop the season change thread
+        System.out.println("Exiting the program. Goodbye!");
+    }
+
+    // Method to show the menu based on the current season
+    public void menuBySeason() {
+        switch (controller.currentSeason()) {
+            case 1:
+                showSpringMenu();
+                break;
+            case 2:
+                showSummerMenu();
+                break;
+            case 3:
+                showFallMenu();
+                break;
+            case 4:
+                showWinterMenu();
+                break;
+            case 5:
+                showYearRoundCrops();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Methods to show the menus for each season
+    private void showSpringMenu() {
+        System.out.println("Menu of Spring Crops:");
+        System.out.println("1. Garlic\n2. Blue Allium\n3. Unmilled Rice\n4. Parsnip");
+    }
+
+    private void showSummerMenu() {
+        System.out.println("Menu of Summer Crops:");
+        System.out.println("1. Poppy\n2. Blueberry\n3. Starfruit\n4. Hot Pepper");
+    }
+
+    private void showFallMenu() {
+        System.out.println("Menu of Fall Crops:");
+        System.out.println("1. Artichoke\n2. Amaranth\n3. Sweet Gem Berry\n4. Eggplant");
+    }
+
+    private void showWinterMenu() {
+        System.out.println("Menu of Winter Crops:");
+        System.out.println("1. Winter Melon");
+
+    }
+
+    public void showYearRoundCrops(){
+        System.out.println("Menu of Year-Round Crops:");
+        System.out.println("1. Ancient Fruit\n2. Fiber Plant\n3. Qi Fruit");
     }
 
     public void createChest(){
